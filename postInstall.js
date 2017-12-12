f30bbe2b68142b0ae66d5487829c4ee862096186
Michael Graf <mi.graf@sap.com>
2017-12-12 11:47:36 +0100

    add dist folder

--- postInstall.js
+++ postInstall.js
@@ -1,4 +1,8 @@
 var fs = require('fs-extra');
 
-fs.copySync('./node_modules/parallax-js/dist/parallax.min.js', './src/openui5/parallax/thirdparty/parallax.min.js');
-fs.copySync('./node_modules/parallax-js/dist/parallax.js', './src/openui5/parallax/thirdparty/parallax.js');
+try {
+	fs.copySync('./node_modules/parallax-js/dist/parallax.min.js', './src/openui5/parallax/thirdparty/parallax.min.js');
+	fs.copySync('./node_modules/parallax-js/dist/parallax.js', './src/openui5/parallax/thirdparty/parallax.js');
+} catch (e) {
+	console.log("an error occured post-processing the openui5.parallax library: " + e.message);
+}
