2aa40aef5de35371bc662c88e061377eb5b0ea67
Merlin Beutlberger <m.beutlberger@sap.com>
2018-10-08 10:50:40 +0200

    Enable for UI5 Tooling usage
    Depends on implementation of UI5 Tooling RFC 2 "Project Shims":
    https://github.com/SAP/ui5-tooling/pull/5

--- postInstall.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs-extra');
-
-try {
-	fs.copySync('./node_modules/parallax-js/dist/parallax.min.js', './src/openui5/parallax/thirdparty/parallax.min.js');
-	fs.copySync('./node_modules/parallax-js/dist/parallax.js', './src/openui5/parallax/thirdparty/parallax.js');
-} catch (e) {
-	console.log("an error occured post-processing the openui5.parallax library: " + e.message);
-}
